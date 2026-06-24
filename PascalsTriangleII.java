b0302bcfa014799438ad5b863650c61676c3e1ec
Rajakumar-852130 <rajk0066kumar@gmail.com>
2026-06-24 00:12:37 -0700

    Added Pascal's Triangle II solution

---
+++ PascalsTriangleII.java
@@ -0,0 +1,66 @@
+// 119. Pascal's Triangle II
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+// Example 1:
+
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+// Example 2:
+
+// Input: rowIndex = 0
+// Output: [1]
+// Example 3:
+
+// Input: rowIndex = 1
+// Output: [1,1]
+ 
+
+// Constraints:
+
+// 0 <= rowIndex <= 33
+ 
+
+// Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+
+
+
+
+
+import java.util.*;
+
+public class PascalsTriangleII {
+
+    public static List<Integer> getRow(int rowIndex) {
+
+        List<Integer> row = new ArrayList<>();
+
+        row.add(1);
+
+        for (int i = 1; i <= rowIndex; i++) {
+
+            row.add(1);
+
+            for (int j = i - 1; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+        }
+
+        return row;
+    }
+
+    public static void main(String[] args) {
+
+        int rowIndex = 3;
+
+        System.out.println(getRow(rowIndex));
+    }
+}
