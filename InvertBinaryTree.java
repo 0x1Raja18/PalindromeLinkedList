50a2e0a2c069e66ab7ca9f30d3bdc5bcef35112d
Rajakumar-852130 <rajk0066kumar@gmail.com>
2026-07-01 05:51:48 -0700

    Added Invert Binary Tree solution

---
+++ InvertBinaryTree.java
@@ -0,0 +1,61 @@
+public class InvertBinaryTree {
+
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static void preorder(TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.val + " ");
+
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(4);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        invertTree(root);
+
+        System.out.println("Preorder Traversal After Inverting:");
+
+        preorder(root);
+    }
+}
